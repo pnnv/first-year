dc2a00824454194791650770b1e1ebc34f0394cb
pranav <pkrokxz@gmail.com>
2023-02-17 18:51:08 +0530

    update

--- cfc/psets/1792A.cpp
+++ cfc/psets/1792A.cpp
@@ -1,1 +1,37 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  map<int, int> m;
+
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+    m[temp]++;
+  }
+  int ans = 0;
+
+  if(m[1] != 0) {
+    if(m[1]&1) {
+      ans += (m[1]/2) + 1;
+    } else {
+      ans += m[1]/2;
+    }
+
+    ans += n - m[1];
+  } else {
+    ans += n;
+  }
+
+  cout << ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
---
+++ cfc/psets/1792B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, ali, bo, d;
+  cin >> a >> ali >> bo >> d;
+
+  int ans = 0;
+
+  if(a != 0) {
+    ans += a + min(ali, bo)*2 + min(a + 1, abs(ali-bo) + d);
+  } else {
+    ans++;
+  }
+
+
+  cout << ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
